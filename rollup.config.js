41f4b771b66cb018e3fea26eb88740bcf628a0eb
Kengo Hamasaki <kengo.hamasaki@indiegogo.com>
2019-05-22 17:42:24 -0700

    Optimize rollup building

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
 import typescript from 'rollup-plugin-typescript2';
-import commonjs from "rollup-plugin-commonjs";
 import pkg from "./package.json";
 
 export default {
   input: "./src/index.ts",
   plugins: [
-    commonjs(),
     typescript({
       clean: true,
       useTsconfigDeclarationDir: true
